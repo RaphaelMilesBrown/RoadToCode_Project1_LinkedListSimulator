bdb1f842d3e9b0d444392ba50303e411d3790f53
RaphaelMilesBrown <rbrown48@gmu.edu>
2023-04-12 15:36:08 -0400

    final commit!

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-
-
 const canvas = document.getElementById('canvas');
 const canvasContext = canvas.getContext("2d");
-
 /*GRADIENT STYLES*/
-
 const rainbowGradientStyle = canvasContext.createLinearGradient(0,0,220,200);
 rainbowGradientStyle.addColorStop("0","red");
 rainbowGradientStyle.addColorStop("0.125","orange");
@@ -15,30 +11,16 @@ rainbowGradientStyle.addColorStop("0.625","blue");
 rainbowGradientStyle.addColorStop("0.75","purple");
 rainbowGradientStyle.addColorStop("0.875","magenta");
 rainbowGradientStyle.addColorStop("1","red");
-
 const monochromeGradientStyle = canvasContext.createLinearGradient(0,0,220,200);
 monochromeGradientStyle.addColorStop("0","black");
 monochromeGradientStyle.addColorStop("1","white");
-
 const sunsetGradientStyle = canvasContext.createLinearGradient(0,0,220,200);
 sunsetGradientStyle.addColorStop("0","red");
 sunsetGradientStyle.addColorStop("1","yellow");
-
 const fillStyleArr = [rainbowGradientStyle, monochromeGradientStyle, sunsetGradientStyle];
-
 /*EXTRA SETUP*/
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-
-const pushFrontButton = document.getElementById("pushFrontButton");
-const pushEndButton = document.getElementById("pushEndButton");
-const popFrontButton = document.getElementById("popFrontButton");
-const popEndButton = document.getElementById("popEndButton");
-
-const pushFrontAction = document.getElementById("pushFrontAction");
-//console.log(pushFrontAction.pushFrontButton);
 class Node
 {
     constructor(pData)
@@ -101,7 +83,6 @@ class LinkedList
             this.tail.nextNode = this.newNode;
             this.tail = this.newNode;
             this.count++;
-            
         }
         else if((this.head != null && this.tail != null) && this.head.nextNode == this.tail)
         {
@@ -118,15 +99,63 @@ class LinkedList
             this.count++;
         }
     }
-    popFront(pData)
+    popFront()
     {
-
+        if(this.head==null)
+        {
+            console.log("INVALID");
+        }
+        else if(this.head != null && this.tail == this.head)
+        {
+            this.head = null;
+            this.tail = null;
+            this.count--;
+        }
+        else if((this.head != null && this.tail != null) && this.head.nextNode == this.tail)
+        {
+            this.head = this.tail;
+            this.head.nextNode = null;
+            this.count--;
+        }
+        else
+        {
+            this.temp = this.head.nextNode;
+            this.head.nextNode = null;
+            this.head = this.temp;
+            this.count--;
+        }
     }
-    popEnd(pData)
+    popEnd()
     {
-
+        if(this.head==null)
+        {
+            console.log("INVALID");
+        }
+        else if(this.head != null && this.tail == this.head)
+        {
+            this.head = null;
+            this.tail = null;
+            this.count--;
+        }
+        else if((this.head != null && this.tail != null) && this.head.nextNode == this.tail)
+        {
+            this.tail = this.head;
+            this.head.nextNode = null;
+            this.count--;
+        }
+        else
+        {
+            this.temp = this.head;
+            for(let i = 0; i < this.count-2; i++)
+            {
+                this.temp = this.temp.nextNode;
+            }
+            this.tail = this.temp;
+            this.tail.nextNode = null;
+            this.temp = null;
+            this.count--;
+        }
     }
-
     displayListV1()
     {
         this.temp = this.head;
@@ -137,79 +166,56 @@ class LinkedList
         }
     }
 }
-
 function drawList(providedList)
 {
-    let drawBoxMover =  -225;
-
-    let drawLocs = 
-    {
-        drawBoxLoc: {x:20,y:20},
-        drawTextLoc: {x:120,y:150},
-    };
-
+    let drawBoxMover = -225;
+    let drawLocs = {drawBoxLoc: {x:20,y:20},drawTextLoc: {x:120,y:150}};
     for(let i = 0; i < providedList.count; i++)
     {
         drawBoxMover+=225;
         providedList.temp = providedList.head;
-        
         for(let j = 0; j <= i; j++)
         {
-            
             /*BOX*/
             canvasContext.fillStyle = fillStyleArr[2];
             canvasContext.lineWidth = 5;
             canvasContext.strokeStyle = 'black'
             canvasContext.fillRect(drawLocs.drawBoxLoc.x + drawBoxMover,drawLocs.drawBoxLoc.y,200,200);
             canvasContext.strokeRect(drawLocs.drawBoxLoc.x + drawBoxMover,drawLocs.drawBoxLoc.y,200,200);
-
             /*TEXT*/
             canvasContext.font = "100px Comic Sans MS";
             canvasContext.textAlign = "center";
             canvasContext.fillStyle = 'black';
             canvasContext.fillText(providedList.temp.data, drawLocs.drawTextLoc.x  + drawBoxMover, drawLocs.drawTextLoc.y);
-
-            /*----------------------------*/
             providedList.temp = providedList.temp.nextNode;
         }
     }
-
 }
+function clearCanvas(){canvasContext.clearRect(0,0,canvas.width,canvas.height);}
 let thisList = new LinkedList();
-pushFrontButton.onclick = function(){drawList(thisList)};
+function pushFrontFunction(providedList, pData){clearCanvas();providedList.pushFront(pData);drawList(providedList);}
+function pushEndFunction(providedList, pData){clearCanvas();providedList.pushEnd(pData);drawList(providedList);}
+function popFrontFunction(providedList){clearCanvas();providedList.popFront();drawList(providedList);}
+function popEndFunction(providedList){clearCanvas();providedList.popEnd();drawList(providedList);}
+document.getElementById("pushFrontButton").onclick = function(){pushFrontFunction(thisList, document.getElementById("pushFrontDataField").value)};
+document.getElementById("pushEndButton").onclick = function(){pushEndFunction(thisList, document.getElementById("pushEndDataField").value)};
+document.getElementById("popFrontButton").onclick = function(){popFrontFunction(thisList)};
+document.getElementById("popEndButton").onclick = function(){popEndFunction(thisList)};
 
-function clearCanvas()
-{
-    canvasContext.clearRect(0,0,canvas.width,canvas.height);
-}
-
-
-thisList.pushEnd(4);
-thisList.pushEnd(5);
-thisList.pushEnd(6);
-
+//const pushFrontAction = document.getElementById("pushFrontAction");
+//console.log(pushFrontAction.pushFrontButton);
 
 //window.setTimeout(function(){drawList(thisList)}, 2000);
 //clearCanvas();
 //canvasContext.clearRect(30,30,10,10);
 
-
-//
-
 /*
-
-
-*/
-/*
-
-
 function pushEndBox()
 {
     
     drawLoc.x += 220;
     clearLoc.x += 220;
 }
-
 function popEndBox()
 {
     canvasContext.clearRect(clearLoc.x,clearLoc.y,220,220);
@@ -218,18 +224,11 @@ function popEndBox()
 }
 */
 
-
 // onclick="pushEndBox()"
 // onclick="popEndBox()"
 
-
-
 //const strokeRectArr = [20,20,200,200];
-
-
 //canvasContext.clearRect(30,30,10,10);
-
-
 
 /*
 canvasContext.fillRect: draws a filled rectangle on canvas.
